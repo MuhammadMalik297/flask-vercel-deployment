0cc79318c90e495f9ed10ebbf2f234c2acb4a42c
MuhammadMalik297 <i212688@nu.edu.pk>
2025-02-19 20:02:36 +0500

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploying Flask App at Vercel"
+    return "Deploying Flask App at Vercel - i21-2688 by Muhammad Malik"
 
 if __name__ == "__main__":
     app.run(debug=True)
